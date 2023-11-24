41144ea374d774c0abca323c9a0c263bfe55ef46
issam-seghir <issamusma@hotmail.com>
2023-11-24 10:53:12 +0100

    rsw grid style

--- src/jsx/pages/Orders.jsx
+++ src/jsx/pages/Orders.jsx
@@ -1,10 +1,11 @@
 import Box from "@mui/joy/Box";
 import Typography from "@mui/joy/Typography";
 import { ColumnDirective, ColumnsDirective, ContextMenu, Edit, ExcelExport, Filter, GridComponent, Inject, Page, PdfExport, Resize, Sort, Toolbar } from "@syncfusion/ej2-react-grids";
-
+import { useMediaQuery } from "@uidotdev/usehooks";
 import { contextMenuItems, ordersData, ordersGrid } from "../data/dummy";
 
 const Orders = () => {
+	const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
 	return (
 		<Box sx={{ p: 2, borderRadius: "5rem" }}>
 			{/* <Header category="Page" title="Orders" /> */}
@@ -18,12 +19,12 @@ const Orders = () => {
 					id="gridcomp"
 					dataSource={ordersData}
 					allowPaging
-					enableStickyHeader
+					enableStickyHeader={isSmallDevice ? false : true}
 					allowSorting
 					allowExcelExport
 					allowPdfExport
-					contextMenuItems={contextMenuItems}
-					editSettings={{ allowDeleting: true, allowEditing: true, allowAdding: true, showDeleteConfirmDialog: true, mode: "Normal" }}
+					contextMenuItems={isSmallDevice ? null : contextMenuItems}
+					editSettings={{ allowDeleting: true, allowEditing: true, allowAdding: true, showDeleteConfirmDialog: true,  mode: `${isSmallDevice ? "Dialog" : "Normal"}` }}
 					allowKeyboard
 					allowFiltering={true}
 					filterSettings={{ type: "Excel" }}
@@ -33,6 +34,8 @@ const Orders = () => {
 					loadingIndicator={{ indicatorType: "Shimmer" }}
 					allowMultiSorting
 					enablePersistence
+					enableAdaptiveUI={isSmallDevice ? true : false}
+					rowRenderingMode={isSmallDevice ? "Vertical" : "Horizontal"}
 				>
 					<ColumnsDirective>
 						{/* eslint-disable-next-line react/jsx-props-no-spreading */}
