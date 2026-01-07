b06ce689e18f0df249032a64b266313b49a25486
issam-seghir <issamusma@hotmail.com>
2026-01-07 11:24:22 +0100

    remove unused vite-aliases dependency and clean up vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import million from "million/compiler";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { ViteAliases } from "vite-aliases";
 import svgr from "vite-plugin-svgr";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
 // import path from "path";
 export default defineConfig(({ command, mode, ssrBuild }) => ({
 	base: "/astralyn/",
@@ -34,30 +35,31 @@ export default defineConfig(({ command, mode, ssrBuild }) => ({
 		// open: (process.env.BROWSER = "E:\\Apps\\scoop\\apps\\googlechrome-dev\\current\\chrome.exe"),
 	},
 	resolve: {
-		extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
-		// ! dont forget to import path
-		/* 		alias: [
-			// { find: "@scss", replacement: path.resolve(__dirname, "src/scss") },
-			// { find: "@", replacement: path.resolve(__dirname, "./src") },
-			// "~gerillass": path.resolve(__dirname, "node_modules/gerillass/scss/gerillass.scss"),
-		], */
+		alias: {
+			"@": resolve(__dirname, "src"),
+			"@scss": resolve(__dirname, "src/scss"),
+			"@jsx": resolve(__dirname, "src/jsx"),
+			"@contexts": resolve(__dirname, "src/jsx/contexts"),
+			"@pages": resolve(__dirname, "src/jsx/pages"),
+			"@components": resolve(__dirname, "src/jsx/components"),
+			"@data": resolve(__dirname, "src/jsx/data"),
+			"@utils": resolve(__dirname, "src/jsx/utils"),
+			"@assets": resolve(__dirname, "src/assets"),
+			"~gerillass": resolve(__dirname, "node_modules/gerillass/scss/gerillass.scss"),
+		},
 	},
+	// resolve: {
+	// 	extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+	// 	// ! dont forget to import path
+	// 	/* 		alias: [
+	// 		// { find: "@scss", replacement: path.resolve(__dirname, "src/scss") },
+	// 		// { find: "@", replacement: path.resolve(__dirname, "./src") },
+	// 		// "~gerillass": path.resolve(__dirname, "node_modules/gerillass/scss/gerillass.scss"),
+	// 	], */
+	// },
 	plugins: [
 		million.vite({ auto: true }),
 		[react()],
-		ViteAliases({
-			dir: "src",
-			prefix: "@",
-			deep: true,
-			depth: 2,
-			adjustDuplicates: true,
-			dts: false,
-			useConfig: true,
-			/**
-			 * Disables any terminal output
-			 */
-			silent: false,
-		}),
 		svgr({
 			// svgr options: https://react-svgr.com/docs/options/
 			svgrOptions: {
